1e8d5d35f2fdaa97b6cf5650a0f32fde80be4266
Chais Robinson <160637164+ChaisRobinson@users.noreply.github.com>
2024-05-19 15:53:25 -0400

    Update: Added scrolling affect iamge to homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Api from './pages/Api';
-import Homepage from './pages/Homepage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ParallaxProvider } from 'react-scroll-parallax';
+import Api from "./pages/Api";
+import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/api" element={<Api />} />
-        </Routes>
-        <Footer />
-      </div>
-   
+    <Router>
+      <ParallaxProvider>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/api" element={<Api />} />
+          </Routes>
+          <Footer />
+        </div>
+      </ParallaxProvider>
+    </Router>
   );
 }
 
